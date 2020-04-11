436b78e585c6e72480911731559bd476474170c6
Nancy <chemutain@gmail.com>
2020-04-11 15:34:10 +0300

    edit regions fuction

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,23 +2,27 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyGdxGame extends ApplicationAdapter {
 	private SpriteBatch batch;
-	private Texture img;
-	private Sprite sprite;//used to convert image
+	private Texture flowerAtlas;
+	private Animation animation;
+	private float timepassed=0;
+//	private Texture img;
+//	private Sprite sprite;//used to convert image
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("circle.png");
-		sprite = new Sprite(img);//convert image in to sprite
+		flowerAtlas = new Texture(Gdx.files.internal("flower.atlas"));
+		animation = new Animation(1/3f, flowerAtlas.getRegions());
+//		img = new Texture("circle.png");
+//		sprite = new Sprite(img);//convert image in to sprite
 	}
 
 	@Override
@@ -28,7 +32,9 @@ public class MyGdxGame extends ApplicationAdapter {
 
 //		start batch to execute the code
 		batch.begin();
-		sprite.draw(batch);
+		timepassed = Gdx.graphics.getDeltaTime();
+		batch.draw((Texture) animation.getKeyFrame(timepassed, true), 100, 100);
+//		sprite.draw(batch);
 		batch.end();
 	}
 	
@@ -36,6 +42,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void dispose () {
 		//dispose resources
 		batch.dispose();
-		img.dispose();
+		flowerAtlas.dispose();
+//		img.dispose();
 	}
 }
