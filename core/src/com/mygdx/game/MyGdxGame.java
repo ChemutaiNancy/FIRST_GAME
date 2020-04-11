0c83f94cac15fae8aa1d9288bef61f1a14c448f8
Nancy <chemutain@gmail.com>
2020-04-11 12:21:27 +0300

    New changes

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,32 +2,38 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+		font.setColor(Color.BLUE);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1,1,1,1);//clear screen color
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+//		start batch to execute the code
 		batch.begin();
-		batch.draw(img, 0, 0);
+		font.draw(batch, "Hello World", 100, 400);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
+		//dispose resources
 		batch.dispose();
-		img.dispose();
+		font.dispose();
 	}
 }
